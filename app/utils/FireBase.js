319132a9f4f1036bb09d48ddefe1ad8379ccfbae
irving <irving14788@gmail.com>
2019-08-08 08:17:42 -0500

    first

---
+++ app/utils/FireBase.js
@@ -0,0 +1,9 @@
+export default (FirebaseConfig = {
+  apiKey: "AIzaSyAfUb2AEW6vUiCJN9ZzNpIHgNcqpnT_KQw",
+  authDomain: "cucharas-754de.firebaseapp.com",
+  databaseURL: "https://cucharas-754de.firebaseio.com",
+  projectId: "cucharas-754de",
+  storageBucket: "cucharas-754de.appspot.com",
+  messagingSenderId: "1081324906368",
+  appId: "1:1081324906368:web:306f27085173cbb8"
+});
